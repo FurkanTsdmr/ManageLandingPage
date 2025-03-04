cc8c01aa8044394dc242d72698802855e49d85bf
FurkanTsdmr <furkantsdmr97@gmail.com>
2025-03-04 21:26:11 +0300

    project-end

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,26 @@
-module.export={
-    content:["./.html"],
-    theme:{
-        extend:{
-            screen:{
-                sm:"480px",
-                md:"768px",
-                lg:"967px",
-                xl:"1440px",
-            },
-            colors:{
-                brightRed:"#ff6347",
-                brightRedLight:"#f6866a",
-                brightRedSupLight:"#fdece7",
-                darkBlue:"#242d52",
-                darkGrayishBlue:"#9095a7",
-                veryDarkBlue:"#1d1e25",
-                veryPaleRed:"#ffefeb",
-                veryLightGray:"#fafafa"
-            },
+
+module.export = {
+    mode:'jit',
+    content: ["./.html"],
+    theme: {
+      extend: {
+        screen:{
+          sm:"480px",
+          md:"768px",
+          lg:"967px",
+          xl:"1440px",
         },
+        colors:{
+          brightRed:"#ff6347",
+          brightRedLight:"#f6866a",
+          brightRedSupLight:"#fdece7",
+          darkBlue:"#242d52",
+          darkGrayishBlue:"#9095a7",
+          veryDarkBlue:"#1d1e25",
+          veryPaleRed:"#ffefeb",
+          veryLightGray:"#fafafa"
+        }
+      },
     },
-}
+    plugins: [],
+  }
